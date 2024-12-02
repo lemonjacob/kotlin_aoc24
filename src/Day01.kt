b17ea6b1fd71a5070fe85689cf3e30837d0585b2
jacobmazza <lemon@lemons.page>
2024-12-02 14:25:51 -0500

    day2 started

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,44 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var numsafe = 0
+        for (line in input) {
+            val floors = line.split(" ")
+            var less = true;
+            var more = true;
+            var safe = true;
+            for (f in 0 until floors.size -1) {
+                if (floors[f] >floors[f+1]) {
+                    more = false;
+                }
+                if (floors[f]<floors[f+1]){
+                    less = false;
+                }
+
+            }
+            if (less || more){
+                for(f in 0 until floors.size-1) {
+                    if (abs(floors[f].toInt()-floors[f+1].toInt()) !in 1..3){
+                        safe = false;
+                    }
+                }
+            }else{
+                safe = false;
+            }
+            if (safe){
+                numsafe += 1;
+            }
+        }
+        return numsafe;
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("input")
     part1(input).println()
     part2(input).println()
 }
---
+++ src/Day02.kt
@@ -0,0 +1,11 @@
+fun main(){
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+    val input = readInput("input")
+    println(part1(input))
+}
