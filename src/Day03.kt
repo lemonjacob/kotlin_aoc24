562d69dada7f9382624cbf7d783227fc5d3c5492
Jacob <jacobjmazza@gmail.com>
2024-12-03 08:02:49 -0500

    day3 started

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,6 +1,10 @@
+import kotlin.text.Regex
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        val regex = Regex("mul\\(\\d+,\\d+\\)")
+        println(regex.findAll(input).first())
+        return 1
     }
 
     fun part2(input: List<String>): Int {
@@ -8,14 +12,11 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
+    val testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
     check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+
 }
